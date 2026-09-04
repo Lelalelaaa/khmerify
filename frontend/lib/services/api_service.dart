42bd0e7e7729641f8f0b5d4f83dae99da49a9980
Senghong <senghongchau@gmail.com>
2026-09-04 15:58:59 +0700

    Fix api_service.dart networking for Android Emulators

--- frontend/lib/services/api_service.dart
+++ frontend/lib/services/api_service.dart
@@ -1,9 +1,20 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
+
 class ApiService {
-  // Change this
-  static const String baseUrl = "http://127.0.0.1:8000";
+  // Use a dynamic getter to choose the correct localhost IP
+  static String get baseUrl {
+    if (kIsWeb) {
+      return "http://127.0.0.1:8000"; // Web
+    } else if (Platform.isAndroid) {
+      return "http://10.0.2.2:8000"; // Android Emulator
+    } else {
+      return "http://127.0.0.1:8000"; // Fallback
+    }
+  }
 
   static Future<String> convert(String input) async {
     final response = await http.post(
