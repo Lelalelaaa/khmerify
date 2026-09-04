99264d2bffae947cbe1f08936f72810ab3c998c6
Faid00 <phumintx@gmail.com>
2026-09-04 20:25:07 +0700

    .

---
+++ frontend/.widget_preview/lib/src/dtd/dtd_connection_info.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: implementation_imports
+
+const String kWidgetPreviewDtdUri = 'ws://127.0.0.1:56348/NZMaNnCzqs4=';
+const String kWidgetPreviewService =
+    'widget-preview-b63adead-61a7-49cc-bc88-5b356fc1e186';
+const String kWidgetPreviewScaffoldStream =
+    'WidgetPreviewScaffold-b63adead-61a7-49cc-bc88-5b356fc1e186';
+const String kProjectRootPath = r'D:\Flutter\khmerify\frontend';
